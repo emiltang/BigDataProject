f6345f2042d306327daccf0f373a340507d21572
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2020-12-03 13:13:48 +0100

    Update GeoMapping.scala

--- ScalaTest/src/main/scala/GeoMapping.scala
+++ ScalaTest/src/main/scala/GeoMapping.scala
@@ -6,34 +6,84 @@ import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
+
 object GeoMapping {
+
+  val topics = Array("twitter")
+
+  val kafkaParams = Map[String, Object](
+    "bootstrap.servers" -> "localhost:9092",
+    "key.deserializer" -> classOf[StringDeserializer],
+    "value.deserializer" -> classOf[StringDeserializer],
+    "group.id" -> "use_a_separate_group_id_for_each_stream",
+    "auto.offset.reset" -> "earliest"
+    // "enable.auto.commit" -> (false: java.lang.Boolean)
+  )
 
   def main(args: Array[String]): Unit = {
 
-    val kafkaParams = Map[String, Object](
-      "bootstrap.servers" -> "localhost:9092,anotherhost:9092",
-      "key.deserializer" -> classOf[StringDeserializer],
-      "value.deserializer" -> classOf[StringDeserializer],
-      "group.id" -> "use_a_separate_group_id_for_each_stream",
-      "auto.offset.reset" -> "latest",
-      "enable.auto.commit" -> (false: java.lang.Boolean)
+    val schema = StructType(
+      StructField(
+        "data",
+        StructType(
+          StructField("id", StringType),
+          StructField("text", StringType),
+          StructField("author_id", StringType)
+        )
+      ),
+      StructField(
+        "includes",
+        StructType(
+          StructType(
+            "users",
+            List(
+              StructType(
+                StructField("id", StringType),
+                StructField("name", StringType),
+                StructField("location", StringType),
+                StructField("usrname", StringType)
+              )
+            )
+          )
+        )
+      ),
+      StructField(
+        "matching_rules",
+        List(
+          StructType(
+            StructField("id", LongType),
+            StructField("tag", StringType, true)
+          )
+        )
+      )
     )
 
-    val topics = Array("twitter")
+    val sparkConf = new SparkConf()
+      .setAppName("GeoMapping")
+      .setMaster("spark://node-master:7077")
 
-
-    val sparkConf = new SparkConf().setAppName("GeoMapping").setMaster("local[2]")
-
-    val streamingContext = new StreamingContext(sparkConf, Seconds(2))
+    val streamingContext = new StreamingContext(sparkConf, Seconds(10))
 
     val stream = KafkaUtils.createDirectStream[String, String](
       streamingContext,
       PreferConsistent,
       Subscribe[String, String](topics, kafkaParams)
     )
+    println("--- begin ---")
 
-    stream.foreachRDD(rdd => {
-      rdd.foreach(print)
-    })
+    stream.map(record => "key" + record.key() + " value" + record.value())
+
+    //stream.foreachRDD(rdd => rdd.foreach(item => println(item)))
+
+    stream.print()
+
+    //stream.print()
+
+    streamingContext.start()
+    streamingContext.awaitTermination()
+
+    println("hello world")
+    println("---  end  ---")
   }
+
 }
